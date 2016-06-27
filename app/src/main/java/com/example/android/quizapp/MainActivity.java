03997322061addffe9d18cb0d43b755eed4d1055
Michael Dijamco <mdd233@cornell.edu>
2016-06-27 14:49:31 -0400

    docs: Comment methods in MainActivity

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -17,23 +17,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /*
+         * Submit Button onClickListener
+         */
         Button submitButton = (Button)findViewById(R.id.submit_button);
         if (submitButton != null) {
             submitButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    // Grade the quiz, generate the message...
                     String msg = gradeQuizMessage();
+                    // ...Display the message in a toast
                     Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
                 }
             });
         }
     }
 
+    /**
+     * Grade question 1
+     * Correct answer: July 4, 1776
+     * @return Question 1 is correct
+     */
     public boolean checkQ1(){
         RadioButton q1Correct = (RadioButton)findViewById(R.id.q1_correct_radio);
         return q1Correct.isChecked();
     }
 
+    /**
+     * Generate question 1 grade string
+     * @return grade string for question 1
+     */
     public String q1Message(){
         if (checkQ1()){
             return "\n1. Correct! The Declaration of Independence was signed on July 4, 1776.\n";
@@ -42,6 +56,11 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Grade question 2
+     * Correct answers: George Washington, George Washington
+     * @return Question 2 is correct
+     */
     public boolean checkQ2(){
         CheckBox q2Correct1 = (CheckBox)findViewById(R.id.q2_correct_checkbox_1);
         CheckBox q2Correct2 = (CheckBox)findViewById(R.id.q2_correct_checkbox_2);
@@ -51,6 +70,10 @@ public class MainActivity extends AppCompatActivity {
         return q2Correct1.isChecked() && q2Correct2.isChecked() && !q2Incorrect1.isChecked() && !q2Incorrect2.isChecked();
     }
 
+    /**
+     * Generate question 2 grade string
+     * @return grade string for question 2
+     */
     public String q2Message(){
         if (checkQ2()){
             return "\n2. Correct! Alexander Hamilton and Ben Franklin were Founding Fathers, but never presidents.\n";
@@ -59,6 +82,11 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Grade question 3
+     * Correct answers (in any order): religion, speech, press, assembly, petition
+     * @return Question 3 is correct
+     */
     public boolean checkQ3(){
         EditText q3AnswerEditText = (EditText)findViewById(R.id.q3_edittext);
         String q3Answer = q3AnswerEditText.getText().toString().toLowerCase();
@@ -70,6 +98,10 @@ public class MainActivity extends AppCompatActivity {
                 && q3Answer.contains("petition");
     }
 
+    /**
+     * Generate question 3 grade string
+     * @return grade string for question 3
+     */
     public String q3Message(){
         if (checkQ3()){
             return "\n3. Correct! USA! USA! USA!\n";
@@ -78,11 +110,20 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Grade question 4
+     * Correct answer: Philadelphia, PA
+     * @return Question 4 is correct
+     */
     public boolean checkQ4(){
         RadioButton q4Correct = (RadioButton)findViewById(R.id.q4_correct_radio);
         return q4Correct.isChecked();
     }
 
+    /**
+     * Generate question 4 grade string
+     * @return grade string for question 4
+     */
     public String q4Message(){
         if (checkQ4()){
             return "\n4. Correct! The First Continental Congress met in Philadelphia, PA.\n";
@@ -91,6 +132,11 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Grade question 5
+     * Correct answers: Executive, Legislative, Judicial
+     * @return Question 5 is correct
+     */
     public boolean checkQ5(){
         CheckBox q5Correct1 = (CheckBox)findViewById(R.id.q5_correct_checkbox_1);
         CheckBox q5Correct2 = (CheckBox)findViewById(R.id.q5_correct_checkbox_2);
@@ -99,6 +145,10 @@ public class MainActivity extends AppCompatActivity {
         return q5Correct1.isChecked() && q5Correct2.isChecked() && q5Correct3.isChecked() && !q5Incorrect.isChecked();
     }
 
+    /**
+     * Generate question 5 grade string
+     * @return grade string for question 5
+     */
     public String q5Message(){
         if (checkQ5()){
             return "\n5. Correct!  There is no Financial branch, but the Federal Reserve is the central bank of the US.\n";
@@ -107,6 +157,10 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Generate grade string for all 5 questions
+     * @return the grade string for all 5 questions
+     */
     public String gradeQuizMessage(){
         return q1Message() + q2Message() + q3Message() + q4Message() + q5Message();
     }
