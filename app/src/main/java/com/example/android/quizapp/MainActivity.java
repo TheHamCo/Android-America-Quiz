bd32d79cd8403b4cd8d27eeac43b5e417da55150
Michael Dijamco <mdd233@cornell.edu>
2016-06-27 14:34:08 -0400

    refactor: Make Toast message methods

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -22,36 +22,7 @@ public class MainActivity extends AppCompatActivity {
             submitButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    String msg = "";
-                    if (checkQ1()){
-                        msg += "\n1. Correct! The Declaration of Independence was signed on July 4, 1776.\n";
-                    } else {
-                        msg += "\n1. Try again!\n";
-                    }
-
-                    if (checkQ2()){
-                        msg += "\n2. Correct! Alexander Hamilton and Ben Franklin were Founding Fathers, but never presidents.\n";
-                    } else {
-                        msg += "\n2. Try again!\n";
-                    }
-
-                    if (checkQ3()){
-                        msg += "\n3. Correct! USA! USA! USA!\n";
-                    } else {
-                        msg += "\n3. Try Again!\n";
-                    }
-
-                    if (checkQ4()){
-                        msg += "\n4. Correct! The First Continental Congress met in Philadelphia, PA.\n";
-                    } else {
-                        msg += "\n4. Try again!\n";
-                    }
-
-                    if (checkQ5()){
-                        msg += "\n5. Correct!  There is no Financial branch, but the Federal Reserve is the central bank of the US.\n";
-                    } else {
-                        msg += "\n5. Try again!\n";
-                    }
+                    String msg = gradeQuizMessage();
                     Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
                 }
             });
@@ -63,6 +34,14 @@ public class MainActivity extends AppCompatActivity {
         return q1Correct.isChecked();
     }
 
+    public String q1Message(){
+        if (checkQ1()){
+            return "\n1. Correct! The Declaration of Independence was signed on July 4, 1776.\n";
+        } else {
+            return "\n1. Try again!\n";
+        }
+    }
+
     public boolean checkQ2(){
         CheckBox q2Correct1 = (CheckBox)findViewById(R.id.q2_correct_checkbox_1);
         CheckBox q2Correct2 = (CheckBox)findViewById(R.id.q2_correct_checkbox_2);
@@ -70,6 +49,14 @@ public class MainActivity extends AppCompatActivity {
         CheckBox q2Incorrect2 = (CheckBox)findViewById(R.id.q2_incorrect_checkbox_2);
 
         return q2Correct1.isChecked() && q2Correct2.isChecked() && !q2Incorrect1.isChecked() && !q2Incorrect2.isChecked();
+    }
+
+    public String q2Message(){
+        if (checkQ2()){
+            return "\n2. Correct! Alexander Hamilton and Ben Franklin were Founding Fathers, but never presidents.\n";
+        } else {
+            return "\n2. Try again!\n";
+        }
     }
 
     public boolean checkQ3(){
@@ -83,9 +70,25 @@ public class MainActivity extends AppCompatActivity {
                 && q3Answer.contains("petition");
     }
 
+    public String q3Message(){
+        if (checkQ3()){
+            return "\n3. Correct! USA! USA! USA!\n";
+        } else {
+            return "\n3. Try Again!\n";
+        }
+    }
+
     public boolean checkQ4(){
         RadioButton q4Correct = (RadioButton)findViewById(R.id.q4_correct_radio);
         return q4Correct.isChecked();
+    }
+
+    public String q4Message(){
+        if (checkQ4()){
+            return "\n4. Correct! The First Continental Congress met in Philadelphia, PA.\n";
+        } else {
+            return "\n4. Try again!\n";
+        }
     }
 
     public boolean checkQ5(){
@@ -94,5 +97,17 @@ public class MainActivity extends AppCompatActivity {
         CheckBox q5Correct3 = (CheckBox)findViewById(R.id.q5_correct_checkbox_3);
         CheckBox q5Incorrect = (CheckBox)findViewById(R.id.q5_incorrect_checkbox);
         return q5Correct1.isChecked() && q5Correct2.isChecked() && q5Correct3.isChecked() && !q5Incorrect.isChecked();
+    }
+
+    public String q5Message(){
+        if (checkQ5()){
+            return "\n5. Correct!  There is no Financial branch, but the Federal Reserve is the central bank of the US.\n";
+        } else {
+            return "\n5. Try again!\n";
+        }
+    }
+
+    public String gradeQuizMessage(){
+        return q1Message() + q2Message() + q3Message() + q4Message() + q5Message();
     }
 }
