7f02f33d0995771ad70534e727069d2a654cdf19
Michael Dijamco <mdd233@cornell.edu>
2016-06-27 13:57:51 -0400

    feat: Implement Question 1

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,9 +19,20 @@ public class MainActivity extends AppCompatActivity {
             submitButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Toast.makeText(getApplicationContext(), "Test", Toast.LENGTH_LONG).show();
+                    String msg = "";
+                    if (checkQ1()){
+                        msg += "\n1. Correct! The Declaration of Independence was signed on July 4, 1776.";
+                    } else {
+                        msg += "\n1. Try again!";
+                    }
+                    Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
                 }
             });
         }
+    }
+
+    public boolean checkQ1(){
+        RadioButton q1Correct = (RadioButton)findViewById(R.id.q1_radio);
+        return q1Correct.isChecked();
     }
 }
